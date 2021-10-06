c14ef25fc6407b92eaeecfdcd4210dcd591c1a1d
andersmorken <andersmorken@gmail.com>
2021-10-06 12:12:05 +0200

    OK

--- BankProject/src/InputLogic.java
+++ BankProject/src/InputLogic.java
@@ -24,3 +24,5 @@ public class InputLogic {
 
   }*/
 }
+
+OK
--- BankProject/src/Main.java
+++ BankProject/src/Main.java
@@ -1,15 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        /*Account currentAccount = new Account("Main Account", "Anders Morken", "1337" );
-        System.out.println(currentAccount.getCustomerName());*/
-
-        Account newAccount = new Account("Second Account", "Vebjørn", "2301");
-
-//        InputLogic input1 = new InputLogic();
-
-
+        Account currentAccount = new Account("Main Account", "Anders Morken", "1337" );
+        System.out.println(currentAccount.getCustomerId());
+        // Real slim shady
 
 
     }
