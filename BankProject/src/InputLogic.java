cca4a336a046b9f5db935e18632c63872508c821
moshk1 <andersmorken@gmail.com>
2021-10-11 10:42:05 +0200

    working. However, I need to fix the InpuitLogic class with constructor and InputLogic method.

--- BankProject/src/InputLogic.java
+++ BankProject/src/InputLogic.java
@@ -4,13 +4,13 @@
 import java.util.Scanner;
 
 public final class InputLogic {
-  private static Account savingsAccount1;
-  private static TransactionData transData;
+  private Account savingsAccount1;
+  private TransactionData transData;
 
 
-  static void InputLogic() {
-    new Account("Savings Account", "Woobear", "1337");
-    new TransactionData(0, 0);
+  public InputLogic() {
+    Account savingsAccount1 = new Account("Savings Account", "Marita", "111187");
+    TransactionData transData = new TransactionData(0, 0);
 
     char option = '\0';
     Scanner scanner = new Scanner(System.in);
@@ -34,32 +34,38 @@ public final class InputLogic {
       System.out.println();
 
       switch (option) {
-        case 'A' -> {
+        case 'A': {
           System.out.println("======================================");
           System.out.println("Balance = $" + transData.getBalance());
           System.out.println("======================================");
           System.out.println();
+          break;
         }
-        case 'B' -> {
+        case 'B': {
           System.out.println("Enter an amount to deposit: ");
           int amount = scanner.nextInt();
           transData.deposit(amount);
           System.out.println();
+          break;
         }
-        case 'C' -> {
+        case 'C': {
           System.out.println("Enter amount to withdraw: ");
           int amount2 = scanner.nextInt();
           transData.withdraw(amount2);
           System.out.println();
+          break;
         }
-        case 'D' -> {
+        case 'D': {
           System.out.println("======================================");
           transData.getPreviousTransaction();
           System.out.println("======================================");
           System.out.println();
+          break;
         }
-        case 'E' -> System.out.println("======================================");
-        default -> System.out.println("Error: invalid option. Please enter A, B, C, D or E");
+        case 'E': System.out.println("======================================");
+        break;
+
+        default: System.out.println("Error: invalid option. Please enter A, B, C, D or E");
       }
     } while (option != 'E');
     System.out.println("Thank you come again " + savingsAccount1.getCustomerName() + "!");
